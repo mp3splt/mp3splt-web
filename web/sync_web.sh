668b6260fa6135b05fa1cd5a2def4110d1b59dfe
Alexandru Munteanu <m@ioalex.net>
2008-10-02 22:56:46 +0000

    sf.net changed their shell access; se do we

--- web/sync_web.sh
+++ web/sync_web.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-rsync -rv --exclude=.svn --delete ./ io_alex_2004@shell.sf.net:/home/groups/m/mp/mp3splt/htdocs/mp3splt_page/
+rsync -rv --exclude=.svn --delete ./ io_alex_2004,mp3splt@web.sourceforge.net:/home/groups/m/mp/mp3splt/htdocs/mp3splt_page/
+
