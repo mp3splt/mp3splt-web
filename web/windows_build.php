7208ce45ee3c8adde47db811587212a6a8232197
Alexandru Munteanu <m@ioalex.net>
2010-11-22 06:54:38 +0000

    added pcre support for setting tags from filename + updated global packaging + windows cross compiling + updated windows build web page

--- web/windows_build.php
+++ web/windows_build.php
@@ -12,30 +12,21 @@ function create_main_page()
 
 <ol>
 <li>Download
-<a href=\"http://prdownloads.sourceforge.net/mingw/MinGW-5.1.6.exe\">MinGW-5.1.6.exe</a>
+<a href=\"http://prdownloads.sourceforge.net/mingw/mingw-get-inst-20101030.exe\">Mingw-get-inst</a>
 and install it in the directory
 <span class=\"colorspan\">c:\mp3splt_mingw</span></li>
 
-<li>Download 
-<a href=\"http://prdownloads.sourceforge.net/mingw/MSYS-1.0.10.exe\">MSYS-1.0.10.exe</a>
-and install it in the same directory as
-MinGW (<span class=\"colorspan\">c:\mp3splt_mingw</span>).
-
-<p>At the end of the installation, it will ask
-you if you want to continue with the post-install. Select yes, and yes
-again when it ask if you have mingw installed. When it ask you to
-indicate the directory where it is installed, type in <span class=\"colorspan\">c:/mp3splt_mingw</span>
-(yes, with a forward slash as the program prefers it that way).</p>
+<p>Select the following extra components to install: 'MSYS Basic System' and 'MinGW Developer ToolKit'.</p>
 </li>
 
-<li>Download <a href=\"http://prdownloads.sourceforge.net/mingw/msysDTK-1.0.1.exe\">msysDTK-1.0.1.exe</a>
-and install it in the same directory as mingw and msys (<span class=\"colorspan\">c:\mp3splt_mingw</span>)</li>
+<li>Download <a href=\"../dev/libmp3splt_mingw_required_libs.tar.bz2\">libmp3splt_mingw_required_libs.tar.bz2</a>,
+<a href=\"../dev/mp3splt-gtk_mingw_required_libs.tar.bz2\">mp3splt-gtk_mingw_required_libs.tar.bz2</a>,
+<a href=\"../dev/mp3splt-gtk_runtime.tar.bz2\">mp3splt-gtk_runtime.tar.bz2</a>
+and put them in the home directory of mingw (<span class=\"colorspan\">c:\mp3splt_mingw\msys\\1.0\home\XXX\</span> where XXX is the username)</li>
 
-<li>Download <a href=\"https://sourceforge.net/projects/mp3splt/files/dev/libmp3splt_mingw_required_libs.tar.bz2/download\">libmp3splt_mingw_required_libs.tar.bz2</a>,
-<a href=\"https://sourceforge.net/projects/mp3splt/files/dev/mp3splt-gtk_mingw_required_libs.tar.bz2/download\">mp3splt-gtk_mingw_required_libs.tar.bz2</a>,
-<a href=\"https://sourceforge.net/projects/mp3splt/files/dev/mp3splt-gtk_runtime.tar.bz2/download\">mp3splt-gtk_runtime.tar.bz2</a> and put them in the home directory of mingw (<span class=\"colorspan\">c:\mp3splt_mingw\home\XXX\</span> where XXX is the username)</li>
-
-<li>Install <a href=\"http://sourceforge.net/project/showfiles.php?group_id=138498&amp;package_id=151948\">TortoiseSVN</a>. Go in the <span class=\"colorspan\">c:\mp3splt_mingw\home\XXX\</span> directory (with windows explorer) and create a directory called mp3splt-project.
+<li>Install <a
+href=\"http://sourceforge.net/project/showfiles.php?group_id=138498&amp;package_id=151948\">TortoiseSVN</a>.
+Go in the <span class=\"colorspan\">c:\mp3splt_mingw\msys\\1.0\home\XXX\</span> directory (with windows explorer) and create a directory called mp3splt-project.
 </li>
 
 <li>
@@ -66,49 +57,11 @@ command line and the other for the gui)</p>
 </ol>
 
 Info : some sources of the dependencies libraries can be found : 
-<a href=\"https://sourceforge.net/projects/mp3splt/files/dev/libmp3splt_mingw_required_libs_sources.tar.bz2/download\">
+<a href=\"../dev/libmp3splt_mingw_required_libs_sources.tar.bz2\">
 libmp3splt_mingw_required_libs_sources.tar.bz2</a>,
-<a href=\"https://sourceforge.net/projects/mp3splt/files/dev/mp3splt-gtk_mingw_required_libs_runtime_sources.tar.bz2/download\">
+<a href=\"../dev/mp3splt-gtk_mingw_required_libs_runtime_sources.tar.bz2\">
 mp3splt-gtk_mingw_required_libs_runtime_sources.tar.bz2</a>.
 <br /><br />
-
-<!--<div class=\"title\">Cross compiling :</div>
-<p>This section explains how to compile mp3splt-project for Windows on a
-<span style=\"font-weight:bold\">Debian GNU/linux</span> operating system.</p>
-
-<ol>
-
-<li>Install mingw, nsis and wine as root :
-<br />
-<code>apt-get install mingw32 mingw32-binutils mingw32-runtime nsis wine subversion</code>
-</li>
-
-<li>Create the <span class=\"colorspan\">libs</span> directory :
-<br />
-<code>mkdir libs</code>
-</li>
-
-<li>Download <a href=\"http://prdownloads.sourceforge.net/mp3splt/dev/libmp3splt_mingw_required_libs.tar.bz2\">libmp3splt_mingw_required_libs.tar.bz2</a>,
-<a href=\"http://prdownloads.sourceforge.net/mp3splt/dev/mp3splt-gtk_mingw_required_libs.tar.bz2\">mp3splt-gtk_mingw_required_libs.tar.bz2</a>,
-<a
-href=\"http://prdownloads.sourceforge.net/mp3splt/dev/mp3splt-gtk_runtime.tar.bz2\">mp3splt-gtk_runtime.tar.bz2</a> and
-put them in the <span class=\"colorspan\">libs</span> directory</li>
-
-<li>Download mp3splt-project subversion code :
-<br />
-<code>svn co https://mp3splt.svn.sourceforge.net/svnroot/mp3splt/mp3splt-project mp3splt-project</code>
-</li>
-
-<li>Type :
-<br />
-<code>cd mp3splt-project &amp;&amp; make windows_cross_installers</code>
-
-<p>Then, two executable installers will be found in the <span
-class=\"colorspan\">mp3splt-project</span> directory (one for the
-command line and the other for the gui)</p>
-</li>
-
-</ol>-->
 ";
 }
 
