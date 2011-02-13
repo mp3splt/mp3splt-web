9d00c7ee2c018e6dfae84f54ae7245d2bad9d0ab
Alexandru Munteanu <m@ioalex.net>
2011-02-13 23:22:34 +0000

    added skeleton for reprepro debian repository

---
+++ debian_repositories/add_to_repository.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+dir=$1
+
+versions="squeeze wheezy unstable karmic lucid maverick"
+
+for version in $versions;do
+  echo "processing $version ..."
+  changes_files=$(ls $dir/*${version}*.changes)
+  for changes_file in $changes_files;do
+    reprepro -b . include $version $changes_file
+  done
+done
+
+echo "done"
+
---
+++ debian_repositories/sync.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rsync -rv --exclude=.svn --delete ./ io_alex_2004,mp3splt@web.sourceforge.net:/home/groups/m/mp/mp3splt/htdocs/debian_repository/
+
