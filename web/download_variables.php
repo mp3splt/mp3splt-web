18ad76939d9bc7df09a57d8b68ea415ada758c04
Alexandru Munteanu <m@ioalex.net>
2012-05-11 21:37:46 +0000

    more stuff for new release

--- web/download_variables.php
+++ web/download_variables.php
@@ -5,9 +5,9 @@
   //$download_suffix="?download";
   $download_suffix="";
 
-  $libmp3splt_version="0.7.1";
-  $mp3splt_version="2.4.1";
-  $mp3splt_gtk_version="0.7.1";
+  $libmp3splt_version="0.7.2";
+  $mp3splt_version="2.4.2";
+  $mp3splt_gtk_version="0.7.2";
 
   $debian_versions=array(
       "Squeeze" => "(stable)",
@@ -15,10 +15,9 @@
       "Unstable" => "");
 
   $ubuntu_versions=array(
-      //"Karmic" => "(9.10)",
-      "Maverick" => "(10.10)",
       "Natty" => "(11.04)",
-      "Oneiric" => "(11.10)"
+      "Oneiric" => "(11.10)",
+      "Precise" => "(12.04)"
       );
 
 ?>
--- web/home.php
+++ web/home.php
@@ -42,15 +42,15 @@ General Public License version 2</a><br /><br />
 <tr>
 <td class=\"mainpagedownloadtd\" style=\"text-align:center\">
 <div style=\"font-weight:bold\">Libmp3splt</div>
-<div style=\"font-style:italic\">0.7.1</div>
+<div style=\"font-style:italic\">0.7.2</div>
 </td>
 <td class=\"mainpagedownloadtd\" style=\"text-align:center\">
 <div style=\"font-weight:bold\">Mp3splt</div>
-<div style=\"font-style:italic\">2.4.1</div>
+<div style=\"font-style:italic\">2.4.2</div>
 </td>
 <td class=\"mainpagedownloadtd\" style=\"text-align:center\">
 <div style=\"font-weight:bold\">Mp3splt-gtk</div>
-<div style=\"font-style:italic\">0.7.1</div>
+<div style=\"font-style:italic\">0.7.2</div>
 </td>
 </tr>
 
