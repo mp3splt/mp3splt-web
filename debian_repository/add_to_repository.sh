b8c96716b3d51109d6b53c74317dd28469b222cf
Alexandru Munteanu <m@ioalex.net>
2013-07-21 16:46:34 +0000

    updated debian repository with 0.9 and 2.6 releases

--- debian_repository/add_to_repository.sh
+++ debian_repository/add_to_repository.sh
@@ -2,7 +2,7 @@
 
 dir=$1
 
-versions="squeeze wheezy unstable oneiric precise quantal"
+versions="wheezy jessie unstable precise quantal raring"
 
 for version in $versions;do
   echo "processing $version ..."
